e55f3e3cf5f2ba156a2427c518e181f3f8edabde
rydanie <rydanielson18@saintjosephhs.com>
2015-11-16 22:32:34 -0500

    die
    new die program done right

--- src/Die.java
+++ src/Die.java
@@ -1,9 +1,57 @@
+import java.util.Random;
+
+/**
+ * This class simulates a six sided die
+ * @author ryley
+ *
+ */
 
 public class Die {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		private int sides; // number of sides
+		private int value; // the die's value
+		
+		/**
+		 * The constructor performs an initial roll of the die
+		 * @param numSides
+		 */
+		
+		public Die(int numSides)
+		{
+			sides = numSides;
+			roll();
+		}
+		
+		/**
+		 * The roll method simulates the rolling of the die
+		 */
+		
+		public void roll()
+		{
+			//create random object
+			Random roll = new Random();
+			
+			//get a random value for the die
+			value = roll.nextInt(sides)+1;
+		}
+		
+		/**
+		 * getSides method
+		 * @return The number of sides for this die
+		 */
+		
+		public int getSides()
+		{
+			return sides;
+		}
+		
+		/**
+		 * getValue method
+		 * @return The value of the die
+		 */
+		
+		public int getValue()
+		{
+			return value;
+		}
 }
